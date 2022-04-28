c44d3f1db9fdbd12d52d08d6e513c98de6f125a6
Khayman King <khaymanak@gmail.com>
2022-04-28 12:06:40 -0700

    lizard people array created

---
+++ src/Lizard People/array-three.js
@@ -0,0 +1,272 @@
+export const lizardPeople = [
+  {
+    '_id': '626ae41183d12839f79e6c2e',
+    'index': 0,
+    'guid': 'a9c5d205-de23-4499-8ffa-24eacc221fd2',
+    'isActive': false,
+    'balance': '$2,155.10',
+    'picture': 'https://www.placecage.com/gif/400/300',
+    'age': 35,
+    'eyeColor': 'green',
+    'name': 'Brennan Velez',
+    'gender': 'male',
+    'company': 'PLASTO',
+    'email': 'brennanvelez@plasto.com',
+    'phone': '+1 (857) 438-2275',
+    'address': '934 Pilling Street, Aberdeen, Missouri, 5365',
+    'about': 'Veniam aute do nostrud ad tempor quis cillum in aute fugiat Lorem irure. Quis non aute proident est ea. Elit proident aliqua aute qui deserunt amet exercitation exercitation sit ullamco id incididunt. Laborum ad laborum est minim labore excepteur consectetur elit duis commodo ex dolore minim tempor. Adipisicing elit ullamco in culpa proident et in Lorem anim officia enim enim tempor. Pariatur dolore consequat cillum excepteur exercitation elit labore. Excepteur pariatur elit reprehenderit non ut nulla occaecat in.\r\n',
+    'registered': '2019-02-14T02:29:38 +08:00',
+    'latitude': 38.320155,
+    'longitude': 143.825017,
+    'tags': [
+      'fugiat',
+      'esse',
+      'occaecat',
+      'officia',
+      'incididunt',
+      'aliquip',
+      'fugiat'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Stewart Garcia'
+      },
+      {
+        'id': 1,
+        'name': 'Harrington Griffith'
+      },
+      {
+        'id': 2,
+        'name': 'Gale Berry'
+      }
+    ],
+    'greeting': 'Hello, Brennan Velez! You have 3 unread messages.',
+    'favoriteFruit': 'strawberry'
+  },
+  {
+    '_id': '626ae411f3ec6116f6654fee',
+    'index': 1,
+    'guid': '7690cd95-3727-46ae-be16-c83d53fff647',
+    'isActive': false,
+    'balance': '$1,624.69',
+    'picture': 'https://www.placecage.com/gif/200/200',
+    'age': 39,
+    'eyeColor': 'brown',
+    'name': 'Chavez Cannon',
+    'gender': 'male',
+    'company': 'MONDICIL',
+    'email': 'chavezcannon@mondicil.com',
+    'phone': '+1 (822) 452-2456',
+    'address': '100 Duryea Court, Hampstead, Maine, 5049',
+    'about': 'Aliquip labore dolor fugiat laborum aliquip veniam officia aliqua velit laborum eiusmod. Veniam irure eiusmod quis ea aliquip dolor. Quis irure velit in commodo occaecat eu ut deserunt ex nisi consequat veniam nulla eu. Consectetur mollit elit mollit ad et magna elit cillum enim.\r\n',
+    'registered': '2021-12-11T04:36:42 +08:00',
+    'latitude': -88.439522,
+    'longitude': -168.637392,
+    'tags': [
+      'quis',
+      'sunt',
+      'eiusmod',
+      'ut',
+      'cupidatat',
+      'eu',
+      'Lorem'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Kristen Roberson'
+      },
+      {
+        'id': 1,
+        'name': 'Verna Pearson'
+      },
+      {
+        'id': 2,
+        'name': 'Bernadine Blackwell'
+      }
+    ],
+    'greeting': 'Hello, Chavez Cannon! You have 2 unread messages.',
+    'favoriteFruit': 'banana'
+  },
+  {
+    '_id': '626ae411d61dbe043488a0c3',
+    'index': 2,
+    'guid': 'c9bf283f-381b-45ab-985c-90178eed9b8e',
+    'isActive': false,
+    'balance': '$3,582.45',
+    'picture': 'https://www.placecage.com/gif/200/500',
+    'age': 34,
+    'eyeColor': 'brown',
+    'name': 'Diane Horn',
+    'gender': 'female',
+    'company': 'NAMEBOX',
+    'email': 'dianehorn@namebox.com',
+    'phone': '+1 (937) 590-3185',
+    'address': '745 Elmwood Avenue, Mayfair, South Dakota, 8830',
+    'about': 'Aute duis ea deserunt deserunt nostrud esse consequat. Veniam laborum duis aliqua Lorem proident ullamco minim reprehenderit non Lorem occaecat fugiat labore. Nisi ullamco Lorem officia excepteur. Sit nostrud magna irure aliqua irure sint pariatur. Laborum velit ea cillum ut sunt aliquip aliquip adipisicing reprehenderit.\r\n',
+    'registered': '2017-11-30T04:43:13 +08:00',
+    'latitude': 79.523599,
+    'longitude': -84.595809,
+    'tags': [
+      'aliquip',
+      'minim',
+      'dolor',
+      'nostrud',
+      'do',
+      'nisi',
+      'cillum'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Underwood Armstrong'
+      },
+      {
+        'id': 1,
+        'name': 'Mcneil Kemp'
+      },
+      {
+        'id': 2,
+        'name': 'Rush Carey'
+      }
+    ],
+    'greeting': 'Hello, Diane Horn! You have 8 unread messages.',
+    'favoriteFruit': 'strawberry'
+  },
+  {
+    '_id': '626ae4119815a41d929876d4',
+    'index': 3,
+    'guid': 'ca7ef1d0-9877-4f35-b3ec-4d260f8880a8',
+    'isActive': true,
+    'balance': '$3,232.91',
+    'picture': 'https://www.placecage.com/gif/400/300',
+    'age': 40,
+    'eyeColor': 'brown',
+    'name': 'Barlow Stark',
+    'gender': 'male',
+    'company': 'DIGIGEN',
+    'email': 'barlowstark@digigen.com',
+    'phone': '+1 (958) 466-3798',
+    'address': '869 Howard Alley, Robinette, Idaho, 9158',
+    'about': 'Quis nostrud ullamco ea eiusmod aliquip exercitation consectetur excepteur cillum. Velit non ipsum ut aliquip do sit proident ex dolore fugiat dolor minim anim. Ut mollit eiusmod incididunt laborum aute excepteur ipsum. Sint Lorem sit quis cillum qui. Laborum reprehenderit aute proident fugiat irure quis deserunt est do quis excepteur voluptate anim. Aliqua labore labore deserunt ipsum ipsum do deserunt proident Lorem mollit pariatur.\r\n',
+    'registered': '2021-09-11T09:37:12 +07:00',
+    'latitude': -34.725543,
+    'longitude': 38.112654,
+    'tags': [
+      'quis',
+      'amet',
+      'aute',
+      'cillum',
+      'id',
+      'occaecat',
+      'eu'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Annette Wolfe'
+      },
+      {
+        'id': 1,
+        'name': 'Alba Bryant'
+      },
+      {
+        'id': 2,
+        'name': 'Jordan Mcmahon'
+      }
+    ],
+    'greeting': 'Hello, Barlow Stark! You have 8 unread messages.',
+    'favoriteFruit': 'strawberry'
+  },
+  {
+    '_id': '626ae411951ea2866028e92c',
+    'index': 4,
+    'guid': 'b18b269a-9782-4d35-8f40-87e86bebb516',
+    'isActive': true,
+    'balance': '$3,390.87',
+    'picture': 'https://www.placecage.com/gif/100/200',
+    'age': 23,
+    'eyeColor': 'blue',
+    'name': 'Tonia Eaton',
+    'gender': 'female',
+    'company': 'ISOTRONIC',
+    'email': 'toniaeaton@isotronic.com',
+    'phone': '+1 (913) 534-2122',
+    'address': '775 Stillwell Avenue, Dennard, Vermont, 6667',
+    'about': 'Est ad commodo incididunt dolore nulla exercitation nulla irure excepteur veniam dolor. Ipsum cillum magna officia est do enim enim minim ut cupidatat consequat. Laborum eu est aliqua in cupidatat dolor Lorem ipsum id laboris officia ullamco nostrud.\r\n',
+    'registered': '2020-01-03T04:07:02 +08:00',
+    'latitude': -25.49482,
+    'longitude': 121.466312,
+    'tags': [
+      'ex',
+      'ipsum',
+      'ad',
+      'minim',
+      'qui',
+      'commodo',
+      'occaecat'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Valencia Howard'
+      },
+      {
+        'id': 1,
+        'name': 'Twila Horton'
+      },
+      {
+        'id': 2,
+        'name': 'Meagan Patel'
+      }
+    ],
+    'greeting': 'Hello, Tonia Eaton! You have 6 unread messages.',
+    'favoriteFruit': 'banana'
+  },
+  {
+    '_id': '626ae411227a48631eb0f4ea',
+    'index': 5,
+    'guid': '1e977f88-8a68-41c7-9c5f-213635ed71b4',
+    'isActive': false,
+    'balance': '$3,761.91',
+    'picture': 'https://www.placecage.com/gif/200/350',
+    'age': 20,
+    'eyeColor': 'brown',
+    'name': 'Georgina Harris',
+    'gender': 'female',
+    'company': 'ARCHITAX',
+    'email': 'georginaharris@architax.com',
+    'phone': '+1 (854) 436-3245',
+    'address': '657 Cox Place, Florence, New Jersey, 2237',
+    'about': 'Commodo tempor anim in amet est ullamco consequat elit proident quis ipsum incididunt deserunt do. Exercitation nulla do do deserunt occaecat nisi qui officia nulla Lorem magna sint. Excepteur cillum amet aliqua magna consectetur ullamco. Laboris ut quis sint officia sint dolor enim proident exercitation aliqua aute ipsum consequat adipisicing. Veniam est ipsum qui velit.\r\n',
+    'registered': '2018-10-17T08:01:08 +07:00',
+    'latitude': 48.850958,
+    'longitude': 81.227114,
+    'tags': [
+      'cupidatat',
+      'incididunt',
+      'fugiat',
+      'commodo',
+      'reprehenderit',
+      'nisi',
+      'aliqua'
+    ],
+    'friends': [
+      {
+        'id': 0,
+        'name': 'Lela Chen'
+      },
+      {
+        'id': 1,
+        'name': 'Alyce Morin'
+      },
+      {
+        'id': 2,
+        'name': 'Mclean Barron'
+      }
+    ],
+    'greeting': 'Hello, Georgina Harris! You have 1 unread messages.',
+    'favoriteFruit': 'banana'
+  }
+];

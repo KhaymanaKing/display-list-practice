98ec5d0d7eb980376ab7543edd94ce72343c709b
Khayman King <khaymanak@gmail.com>
2022-04-28 10:28:08 -0700

    started candies

---
+++ src/candies/Candies.js
@@ -0,0 +1,22 @@
+export const candy = [
+  {
+    name: 'Snickers',
+    chocolate: 'yes',
+    rating: '6'
+  },
+  {
+    name: 'Skittles',
+    chocolate: 'no',
+    rating: '3'
+  },
+  {
+    name: 'Payday',
+    chocolate: 'no',
+    rating: '8'
+  },
+  {
+    name: 'Sweet Tarts',
+    chocolate: 'no',
+    rating: '1'
+  }
+];
